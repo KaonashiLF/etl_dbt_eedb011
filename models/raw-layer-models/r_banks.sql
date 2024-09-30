366706692184108d42a23a17946254ed258a24de
Lucas Silva <lucasfrancs.contato@gmail.com>
2024-09-30 03:27:48 +0000

    project name renamed

--- models/raw-layer-models/r_banks.sql
+++ models/raw-layer-models/r_banks.sql
@@ -1,2 +1,9 @@
 {{ config(materialized='table') }}
 
+SELECT
+  -- Mapeie as colunas do seu arquivo Parquet para as colunas da tabela
+  coluna_segmento AS SEGMENTO,
+  coluna_cnpj AS CNPJ,
+  coluna_nome AS NOME
+FROM {{ source('etl_dbt_eedb011', 'banks') }}
+WHERE filename = 'EnquadramentoInicia_v2.parquet'
